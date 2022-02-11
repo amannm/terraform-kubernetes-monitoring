ee247cc9169e9a4955668a3740c880abdc8fa8cb
Amann Malik <amannmalik@gmail.com>
2022-02-10 20:38:13 -0600

    try this

--- module/etcd/main.tf
+++ module/etcd/main.tf
@@ -1,42 +1,43 @@
 locals {
   cluster_size            = 3
   client_port             = 2379
+  cluster_domain          = "cluster.local"
   peer_port               = 2380
   data_volume_name        = "data"
   data_volume_mount_path  = "/var/run/etcd"
   service_client_endpoint = "${module.service.non_headless_service_hostname}:${local.client_port}"
   startup_script          = <<-EOT
-  HOSTNAME=$$(hostname)
+  HOSTNAME=$(hostname)
   # store member id into PVC for later member replacement
   collect_member() {
       while ! etcdctl member list &>/dev/null; do sleep 1; done
-      etcdctl member list | grep http://$${HOSTNAME}.${var.service_name}:${local.peer_port} | cut -d':' -f1 | cut -d'[' -f1 > ${local.data_volume_mount_path}/member_id
+      etcdctl member list | grep http://$${HOSTNAME}.${var.namespace_name}.svc.${local.cluster_domain}:${local.peer_port} | cut -d':' -f1 | cut -d'[' -f1 > ${local.data_volume_mount_path}/member_id
       exit 0
   }
   eps() {
       EPS=""
       for i in $$(seq 0 $$((${local.cluster_size} - 1))); do
-          EPS="$${EPS}$${EPS:+,}http://${var.service_name}-$${i}.${var.service_name}:${local.client_port}"
+          EPS="$${EPS}$${EPS:+,}http://${var.service_name}-$${i}.${var.namespace_name}.svc.${local.cluster_domain}:${local.client_port}"
       done
       echo $${EPS}
   }
   member_hash() {
-      etcdctl member list | grep http://$${HOSTNAME}.${var.service_name}:${local.peer_port} | cut -d':' -f1 | cut -d'[' -f1
+      etcdctl member list | grep http://$${HOSTNAME}.${var.namespace_name}.svc.${local.cluster_domain}:${local.peer_port} | cut -d':' -f1 | cut -d'[' -f1
   }
   # re-joining after failure?
   if [ -e ${local.data_volume_mount_path}/default.etcd ]; then
       echo "Re-joining etcd member"
       member_id=$$(cat ${local.data_volume_mount_path}/member_id)
       # re-join member
-      ETCDCTL_ENDPOINT=$$(eps) etcdctl member update $${member_id} http://$${HOSTNAME}.${var.service_name}:${local.peer_port}
+      ETCDCTL_ENDPOINT=$$(eps) etcdctl member update $${member_id} http://$${HOSTNAME}.${var.namespace_name}.svc.${local.cluster_domain}:${local.peer_port}
       exec etcd --name $${HOSTNAME} \
-          --listen-peer-urls http://$${HOSTNAME}.${var.service_name}:${local.peer_port} \
-          --listen-client-urls http://$${HOSTNAME}.${var.service_name}:${local.client_port},http://127.0.0.1:${local.client_port} \
-          --advertise-client-urls http://$${HOSTNAME}.${var.service_name}:${local.client_port} \
+          --listen-peer-urls http://${var.service_name}.${var.namespace_name}.svc.${local.cluster_domain}:${local.peer_port} \
+          --listen-client-urls http://${var.service_name}.${var.namespace_name}.svc.${local.cluster_domain}:${local.client_port},http://127.0.0.1:${local.client_port} \
+          --advertise-client-urls http://${var.service_name}.${var.namespace_name}.svc.${local.cluster_domain}:${local.client_port} \
           --data-dir ${local.data_volume_mount_path}/default.etcd
   fi
   # etcd-SET_ID
-  SET_ID=$${HOSTNAME:5:$${#HOSTNAME}}
+  SET_ID=$${HOSTNAME##*-}
   # adding a new member to existing cluster (assuming all initial pods are available)
   if [ "$${SET_ID}" -ge ${local.cluster_size} ]; then
       export ETCDCTL_ENDPOINT=$$(eps)
@@ -49,7 +50,7 @@ locals {
           etcdctl member remove $${MEMBER_HASH}
       fi
       echo "Adding new member"
-      etcdctl member add $${HOSTNAME} http://$${HOSTNAME}.${var.service_name}:${local.peer_port} | grep "^ETCD_" > ${local.data_volume_mount_path}/new_member_envs
+      etcdctl member add $${HOSTNAME} http://${var.service_name}.${var.namespace_name}.svc.${local.cluster_domain}:${local.peer_port} | grep "^ETCD_" > ${local.data_volume_mount_path}/new_member_envs
       if [ $$? -ne 0 ]; then
           echo "Exiting"
           rm -f ${local.data_volume_mount_path}/new_member_envs
@@ -59,11 +60,11 @@ locals {
       source ${local.data_volume_mount_path}/new_member_envs
       collect_member &
       exec etcd --name $${HOSTNAME} \
-          --listen-peer-urls http://$${HOSTNAME}.${var.service_name}:${local.peer_port} \
-          --listen-client-urls http://$${HOSTNAME}.${var.service_name}:${local.client_port},http://127.0.0.1:${local.client_port} \
-          --advertise-client-urls http://$${HOSTNAME}.${var.service_name}:${local.client_port} \
+          --listen-peer-urls http://${var.service_name}.${var.namespace_name}.svc.${local.cluster_domain}:${local.peer_port} \
+          --listen-client-urls http://${var.service_name}.${var.namespace_name}.svc.${local.cluster_domain}:${local.client_port},http://127.0.0.1:${local.client_port} \
+          --advertise-client-urls http://${var.service_name}.${var.namespace_name}.svc.${local.cluster_domain}:${local.client_port} \
           --data-dir ${local.data_volume_mount_path}/default.etcd \
-          --initial-advertise-peer-urls http://$${HOSTNAME}.${var.service_name}:${local.peer_port} \
+          --initial-advertise-peer-urls http://${var.service_name}.${var.namespace_name}.svc.${local.cluster_domain}:${local.peer_port} \
           --initial-cluster $${ETCD_INITIAL_CLUSTER} \
           --initial-cluster-state $${ETCD_INITIAL_CLUSTER_STATE}
   fi
@@ -81,10 +82,10 @@ locals {
   collect_member &
   # join member
   exec etcd --name $${HOSTNAME} \
-      --initial-advertise-peer-urls http://$${HOSTNAME}.${var.service_name}:${local.peer_port} \
-      --listen-peer-urls http://$${HOSTNAME}.${var.service_name}:${local.peer_port} \
-      --listen-client-urls http://$${HOSTNAME}.${var.service_name}:${local.client_port},http://127.0.0.1:${local.client_port} \
-      --advertise-client-urls http://$${HOSTNAME}.${var.service_name}:${local.client_port} \
+      --initial-advertise-peer-urls http://${var.service_name}.${var.namespace_name}.svc.${local.cluster_domain}:${local.peer_port} \
+      --listen-peer-urls http://${var.service_name}.${var.namespace_name}.svc.${local.cluster_domain}:${local.peer_port} \
+      --listen-client-urls http://${var.service_name}.${var.namespace_name}.svc.${local.cluster_domain}:${local.client_port},http://127.0.0.1:${local.client_port} \
+      --advertise-client-urls http://${var.service_name}.${var.namespace_name}.svc.${local.cluster_domain}:${local.client_port} \
       --initial-cluster-token etcd-cluster-1 \
       --initial-cluster $${PEERS} \
       --initial-cluster-state new \
@@ -93,11 +94,10 @@ locals {
   pre_stop_script         = <<-EOT
   EPS=""
   for i in $$(seq 0 $$((${local.cluster_size} - 1))); do
-      EPS="$${EPS}$${EPS:+,}http://${var.service_name}-$${i}.${var.service_name}:${local.client_port}"
+      EPS="$${EPS}$${EPS:+,}http://${var.service_name}-$${i}.${var.namespace_name}.svc.${local.cluster_domain}:${local.client_port}"
   done
-  HOSTNAME=$$(hostname)
   member_hash() {
-      etcdctl member list | grep http://$${HOSTNAME}.${var.service_name}:${local.peer_port} | cut -d':' -f1 | cut -d'[' -f1
+      etcdctl member list | grep http://${var.service_name}.${var.namespace_name}.svc.${local.cluster_domain}:${local.peer_port} | cut -d':' -f1 | cut -d'[' -f1
   }
   echo "Removing $${HOSTNAME} from etcd cluster"
   ETCDCTL_ENDPOINT=$${EPS} etcdctl member remove $$(member_hash)
