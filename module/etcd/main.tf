9e29806b20eb56553919b0620ceb76d82ccedcc6
Amann Malik <amannmalik@gmail.com>
2022-02-10 19:27:34 -0600

    switch to statefulset for etcd

--- module/etcd/main.tf
+++ module/etcd/main.tf
@@ -1,118 +1,109 @@
 locals {
+  cluster_size            = 3
   client_port             = 2379
   peer_port               = 2380
   data_volume_name        = "data"
   data_volume_mount_path  = "/var/run/etcd"
-  data_volume_host_path   = "/etcd/data"
   service_client_endpoint = "${module.service.non_headless_service_hostname}:${local.client_port}"
-  get_ip                  = "export IP=$(hostname -i)"
-  member_hash_command     = "etcdctl member list | grep http://$${IP}:${local.peer_port} | cut -d':' -f1 | cut -d'[' -f1"
-  get_peers_list          = <<-EOT
-  PEERS=$(nslookup ${module.service.headless_service_hostname} 2>/dev/null | grep Address | awk -F ": " '{print $2}' | grep -v " ")
-  EOT
-  list_peers_function     = <<-EOT
-  list_peers() {
-    EPS=""
-    for i in $${PEERS}; do
-      EPS="$${EPS}$${EPS:+,}http://$${i}:${local.client_port}"
-    done
-    echo $${EPS}
-  }
-  EOT
   startup_script          = <<-EOT
-  mkdir -p ${local.data_volume_mount_path}
-  sleep 5
-  HOSTNAME=$(hostname)
-  ${local.get_ip}
-  ${local.get_peers_list}
-  echo "peers: $PEERS"
-  ${local.list_peers_function}
-  list_servers() {
-    EPS=""
-    for i in $${PEERS}; do
-      EPS="$${EPS}$${EPS:+,}$${i}=http://$${i}:${local.peer_port}"
-    done
-    echo $${EPS},$${IP}=http://$${IP}:${local.peer_port}
-  }
+  HOSTNAME=$$(hostname)
+  # store member id into PVC for later member replacement
   collect_member() {
-    ETCDCTL_ENDPOINT=$(list_peers)
-    while ! etcdctl member list &>/dev/null; do sleep 1; done
-    ${local.member_hash_command} > ${local.data_volume_mount_path}/member_id
-    exit 0
+      while ! etcdctl member list &>/dev/null; do sleep 1; done
+      etcdctl member list | grep http://$${HOSTNAME}.${var.service_name}:${local.peer_port} | cut -d':' -f1 | cut -d'[' -f1 > ${local.data_volume_mount_path}/member_id
+      exit 0
+  }
+  eps() {
+      EPS=""
+      for i in $$(seq 0 $$((${local.cluster_size} - 1))); do
+          EPS="$${EPS}$${EPS:+,}http://${var.service_name}-$${i}.${var.service_name}:${local.client_port}"
+      done
+      echo $${EPS}
   }
   member_hash() {
-    ETCDCTL_ENDPOINT=$(list_peers) ${local.member_hash_command}
+      etcdctl member list | grep http://$${HOSTNAME}.${var.service_name}:${local.peer_port} | cut -d':' -f1 | cut -d'[' -f1
   }
-  check_cluster() {
-    ETCDCTL_ENDPOINT=$(list_peers) etcdctl member list > /dev/null
-    local exit_code=$?
-    echo "$exit_code"
-  }
+  # re-joining after failure?
   if [ -e ${local.data_volume_mount_path}/default.etcd ]; then
-    echo "rejoining cluster"
-    member_id=$(cat ${local.data_volume_mount_path}/member_id)
-    ETCDCTL_ENDPOINT=$(list_peers) etcdctl member update $${member_id} http://$${IP}:${local.peer_port}
-    exec etcd --name $${IP} \
-        --listen-peer-urls http://$${IP}:${local.peer_port} \
-        --listen-client-urls http://$${IP}:${local.client_port},http://127.0.0.1:${local.client_port} \
-        --advertise-client-urls http://$${IP}:${local.client_port} \
-        --data-dir ${local.data_volume_mount_path}/default.etcd
+      echo "Re-joining etcd member"
+      member_id=$$(cat ${local.data_volume_mount_path}/member_id)
+      # re-join member
+      ETCDCTL_ENDPOINT=$$(eps) etcdctl member update $${member_id} http://$${HOSTNAME}.${var.service_name}:${local.peer_port}
+      exec etcd --name $${HOSTNAME} \
+          --listen-peer-urls http://$${HOSTNAME}.${var.service_name}:${local.peer_port} \
+          --listen-client-urls http://$${HOSTNAME}.${var.service_name}:${local.client_port},http://127.0.0.1:${local.client_port} \
+          --advertise-client-urls http://$${HOSTNAME}.${var.service_name}:${local.client_port} \
+          --data-dir ${local.data_volume_mount_path}/default.etcd
   fi
-  echo "checking for existing cluster"
-  CLUSTER=$(check_cluster)
-  if [[ "$CLUSTER" == "0" ]]; then
-    echo "joining existing cluster"
-    MEMBER_HASH=$(member_hash)
-    echo "member hash is $MEMBER_HASH"
-    if [ -n "$${MEMBER_HASH}" ]; then
-        ETCDCTL_ENDPOINT=$(list_peers) etcdctl member remove $${MEMBER_HASH}
-    fi
-    ETCDCTL_ENDPOINT=$(list_peers) etcdctl member add $${IP} http://$${IP}:${local.peer_port} | grep "^ETCD_" > ${local.data_volume_mount_path}/new_member_envs
-    if [ $? -ne 0 ]; then
-      echo "exiting"
-      rm -f ${local.data_volume_mount_path}/new_member_envs
-      exit 1
-    fi
-    cat ${local.data_volume_mount_path}/new_member_envs
-    source ${local.data_volume_mount_path}/new_member_envs
-    collect_member &
-    exec etcd --name $${IP} \
-          --listen-peer-urls http://$${IP}:${local.peer_port} \
-          --listen-client-urls http://$${IP}:${local.client_port},http://127.0.0.1:${local.client_port} \
-          --advertise-client-urls http://$${IP}:${local.client_port} \
+  # etcd-SET_ID
+  SET_ID=$${HOSTNAME:5:$${#HOSTNAME}}
+  # adding a new member to existing cluster (assuming all initial pods are available)
+  if [ "$${SET_ID}" -ge $${INITIAL_CLUSTER_SIZE} ]; then
+      export ETCDCTL_ENDPOINT=$$(eps)
+      # member already added?
+      MEMBER_HASH=$$(member_hash)
+      if [ -n "$${MEMBER_HASH}" ]; then
+          # the member hash exists but for some reason etcd failed
+          # as the datadir has not be created, we can remove the member
+          # and retrieve new hash
+          etcdctl member remove $${MEMBER_HASH}
+      fi
+      echo "Adding new member"
+      etcdctl member add $${HOSTNAME} http://$${HOSTNAME}.${var.service_name}:${local.peer_port} | grep "^ETCD_" > ${local.data_volume_mount_path}/new_member_envs
+      if [ $$? -ne 0 ]; then
+          echo "Exiting"
+          rm -f ${local.data_volume_mount_path}/new_member_envs
+          exit 1
+      fi
+      cat ${local.data_volume_mount_path}/new_member_envs
+      source ${local.data_volume_mount_path}/new_member_envs
+      collect_member &
+      exec etcd --name $${HOSTNAME} \
+          --listen-peer-urls http://$${HOSTNAME}.${var.service_name}:${local.peer_port} \
+          --listen-client-urls http://$${HOSTNAME}.${var.service_name}:${local.client_port},http://127.0.0.1:${local.client_port} \
+          --advertise-client-urls http://$${HOSTNAME}.${var.service_name}:${local.client_port} \
           --data-dir ${local.data_volume_mount_path}/default.etcd \
-          --initial-advertise-peer-urls http://$${IP}:${local.peer_port} \
+          --initial-advertise-peer-urls http://$${HOSTNAME}.${var.service_name}:${local.peer_port} \
           --initial-cluster $${ETCD_INITIAL_CLUSTER} \
           --initial-cluster-state $${ETCD_INITIAL_CLUSTER_STATE}
   fi
-  echo "pinging peers"
-  for i in $PEERS; do
-    while true; do
-      echo "waiting for $${i} to start..."
-      ping -W 1 -c 1 $${i} > /dev/null && break
-      sleep 1s
-    done
+  for i in $$(seq 0 $$(($${INITIAL_CLUSTER_SIZE} - 1))); do
+      while true; do
+          echo "Waiting for ${var.service_name}-$${i}.${var.service_name} to come up"
+          ping -W 1 -c 1 ${var.service_name}-$${i}.${var.service_name} > /dev/null && break
+          sleep 1s
+      done
+  done
+  PEERS=""
+  for i in $$(seq 0 $$(($${INITIAL_CLUSTER_SIZE} - 1))); do
+      PEERS="$${PEERS}$${PEERS:+,}${var.service_name}-$${i}=http://${var.service_name}-$${i}.${var.service_name}:${local.peer_port}"
   done
   collect_member &
-  echo "joining new cluster"
-  exec etcd --name $${IP} \
-          --listen-peer-urls http://$${IP}:${local.peer_port} \
-          --listen-client-urls http://$${IP}:${local.client_port},http://127.0.0.1:${local.client_port} \
-          --advertise-client-urls http://$${IP}:${local.client_port} \
-          --data-dir ${local.data_volume_mount_path}/default.etcd \
-          --initial-advertise-peer-urls http://$${IP}:${local.peer_port} \
-          --initial-cluster $(list_servers) \
-          --initial-cluster-state new \
-          --initial-cluster-token ${var.service_name}-cluster
+  # join member
+  exec etcd --name $${HOSTNAME} \
+      --initial-advertise-peer-urls http://$${HOSTNAME}.${var.service_name}:${local.peer_port} \
+      --listen-peer-urls http://$${HOSTNAME}.${var.service_name}:${local.peer_port} \
+      --listen-client-urls http://$${HOSTNAME}.${var.service_name}:${local.client_port},http://127.0.0.1:${local.client_port} \
+      --advertise-client-urls http://$${HOSTNAME}.${var.service_name}:${local.client_port} \
+      --initial-cluster-token etcd-cluster-1 \
+      --initial-cluster $${PEERS} \
+      --initial-cluster-state new \
+      --data-dir ${local.data_volume_mount_path}/default.etcd
   EOT
   pre_stop_script         = <<-EOT
-  ${local.get_ip}
-  ${local.get_peers_list}
-  ${local.list_peers_function}
-  echo "removing $${IP} from cluster"
-  ETCDCTL_ENDPOINT=$(list_peers) etcdctl member remove $(${local.member_hash_command})
-  if [ $? -eq 0 ]; then
-    rm -rf ${local.data_volume_mount_path}/*
+  EPS=""
+  for i in $$(seq 0 $$((${local.cluster_size} - 1))); do
+      EPS="$${EPS}$${EPS:+,}http://${var.service_name}-$${i}.${var.service_name}:${local.client_port}"
+  done
+  HOSTNAME=$$(hostname)
+  member_hash() {
+      etcdctl member list | grep http://$${HOSTNAME}.${var.service_name}:${local.peer_port} | cut -d':' -f1 | cut -d'[' -f1
+  }
+  echo "Removing $${HOSTNAME} from etcd cluster"
+  ETCDCTL_ENDPOINT=$${EPS} etcdctl member remove $$(member_hash)
+  if [ $$? -eq 0 ]; then
+      # Remove everything otherwise the cluster will no longer scale-up
+      rm -rf ${local.data_volume_mount_path}/*
   fi
   EOT
 }
@@ -133,16 +124,31 @@ module "service" {
   }
 }
 
-resource "kubernetes_daemonset" "daemonset" {
+resource "kubernetes_stateful_set" "stateful_set" {
   metadata {
     name      = var.service_name
     namespace = var.namespace_name
   }
   spec {
-    strategy {
-      type = "RollingUpdate"
+    service_name = var.service_name
+    replicas     = 3
+    update_strategy {
       rolling_update {
-        max_unavailable = 1
+        partition = 0
+      }
+    }
+    volume_claim_template {
+      metadata {
+        name      = local.data_volume_name
+        namespace = var.namespace_name
+      }
+      spec {
+        access_modes = ["ReadWriteOnce"]
+        resources {
+          requests = {
+            storage = "200Mi"
+          }
+        }
       }
     }
     selector {
@@ -158,19 +164,35 @@ resource "kubernetes_daemonset" "daemonset" {
         }
       }
       spec {
-        volume {
-          name = local.data_volume_name
-          empty_dir {
+        affinity {
+          pod_anti_affinity {
+            required_during_scheduling_ignored_during_execution {
+              label_selector {
+                match_labels = {
+                  component = var.service_name
+                }
+              }
+              topology_key = "kubernetes.io/hostname"
+            }
+            preferred_during_scheduling_ignored_during_execution {
+              weight = 100
+              pod_affinity_term {
+                label_selector {
+                  match_labels = {
+                    component = var.service_name
+                  }
+                }
+                topology_key = "failure-domain.beta.kubernetes.io/zone"
+              }
+            }
           }
         }
         container {
+
           name              = var.service_name
           image             = var.container_image
           image_pull_policy = "IfNotPresent"
-          security_context {
-            privileged = true
-          }
-          command = ["/bin/sh", "-ec", local.startup_script]
+          command           = ["/bin/sh", "-ec", local.startup_script]
           lifecycle {
             pre_stop {
               exec {
