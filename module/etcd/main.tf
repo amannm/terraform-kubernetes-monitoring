17ba007aef620feeaf3ebae62bf0e214bf6ced08
Amann Malik <amannmalik@gmail.com>
2022-02-16 22:09:47 -0600

    try this

--- module/etcd/main.tf
+++ module/etcd/main.tf
@@ -5,20 +5,24 @@ locals {
   data_volume_name        = "data"
   data_volume_mount_path  = "/var/run/etcd"
   service_client_endpoint = "${module.service.non_headless_service_hostname}:${local.client_port}"
-
-  script_globals = <<-EOT
-  PEER_IPS=$(nslookup ${module.service.headless_service_hostname} 2>/dev/null | grep Address | awk -F ": " '{print $2}' | grep -v " ")
-  echo "peer IPs: $PEER_IPS"
-  ALL_CLIENT_ENDPOINTS=""
-  for peer_ip in $${PEER_IPS}; do
-      ALL_CLIENT_ENDPOINTS="$${ALL_CLIENT_ENDPOINTS}$${ALL_CLIENT_ENDPOINTS:+,}http://$${peer_ip}:${local.client_port}"
-  done
-  echo "client endpoints: $ALL_CLIENT_ENDPOINTS"
+  domain_suffix           = "${var.namespace_name}.svc.${local.cluster_domain}"
+  script_globals          = <<-EOT
+  SET_ID="$${POD_NAME##*-}"
+  SET_NAME="$${POD_NAME%%-*}"
+  HOSTNAME=$${POD_NAME}.${local.domain_suffix}
   IP=$(hostname -i)
+  ALL_CLIENT_ENDPOINTS=""
+  for i in $(seq 0 $((${SET_ID} - 1))); do
+      ALL_CLIENT_ENDPOINTS="$${ALL_CLIENT_ENDPOINTS}$${ALL_CLIENT_ENDPOINTS:+,}http://$${SET_NAME}-$${i}.${local.domain_suffix}:${local.client_port}"
+  done
+  echo "SET_ID: $SET_ID"
+  echo "SET_NAME: $SET_NAME"
+  echo "HOSTNAME: $HOSTNAME"
   echo "IP: $IP"
+  echo "ALL_CLIENT_ENDPOINTS: $ALL_CLIENT_ENDPOINTS"
   EOT
 
-  get_member_id = "ETCDCTL_ENDPOINT=\"$ALL_CLIENT_ENDPOINTS\" etcdctl member list | grep http://$${IP}:${local.peer_port} | cut -d':' -f1 | cut -d'[' -f1"
+  get_member_id = "ETCDCTL_ENDPOINT=\"$ALL_CLIENT_ENDPOINTS\" etcdctl member list | grep http://$${HOSTNAME}:${local.peer_port} | cut -d':' -f1 | cut -d'[' -f1"
 
   startup_script = <<-EOT
   ${local.script_globals}
@@ -34,11 +38,11 @@ locals {
       echo "existing cluster found"
       if [ -e ${local.data_volume_mount_path}/default.etcd ]; then
           echo "re-joining existing cluster as existing member"
-          ETCDCTL_ENDPOINT="$ALL_CLIENT_ENDPOINTS" etcdctl member update $(cat ${local.data_volume_mount_path}/member_id) http://$${IP}:${local.peer_port}
-          exec etcd --name $${IP} \
+          ETCDCTL_ENDPOINT="$ALL_CLIENT_ENDPOINTS" etcdctl member update $(cat ${local.data_volume_mount_path}/member_id) http://$${HOSTNAME}:${local.peer_port}
+          exec etcd --name $${POD_NAME} \
               --listen-peer-urls http://$${IP}:${local.peer_port} \
               --listen-client-urls http://$${IP}:${local.client_port},http://127.0.0.1:${local.client_port} \
-              --advertise-client-urls http://$${IP}:${local.client_port} \
+              --advertise-client-urls http://$${HOSTNAME}:${local.client_port} \
               --data-dir ${local.data_volume_mount_path}/default.etcd
       else
           MEMBER_ID=$(${local.get_member_id})
@@ -47,7 +51,7 @@ locals {
               ETCDCTL_ENDPOINT="$ALL_CLIENT_ENDPOINTS" etcdctl member remove $${MEMBER_ID}
           fi
           echo "registering with existing cluster as new member"
-          ETCDCTL_ENDPOINT="$ALL_CLIENT_ENDPOINTS" etcdctl member add $${IP} http://$${IP}:${local.peer_port} | grep "^ETCD_" > ${local.data_volume_mount_path}/new_member_envs
+          ETCDCTL_ENDPOINT="$ALL_CLIENT_ENDPOINTS" etcdctl member add $${POD_NAME} http://$${HOSTNAME}:${local.peer_port} | grep "^ETCD_" > ${local.data_volume_mount_path}/new_member_envs
           if [ $? -ne 0 ]; then
               echo "failed to register with existing cluster"
               rm -f ${local.data_volume_mount_path}/new_member_envs
@@ -57,30 +61,26 @@ locals {
           source ${local.data_volume_mount_path}/new_member_envs
           save_member_id &
           echo "joining existing cluster"
-          exec etcd --name $${IP} \
+          exec etcd --name $${POD_NAME} \
               --listen-peer-urls http://$${IP}:${local.peer_port} \
               --listen-client-urls http://$${IP}:${local.client_port},http://127.0.0.1:${local.client_port} \
-              --advertise-client-urls http://$${IP}:${local.client_port} \
+              --advertise-client-urls http://$${HOSTNAME}:${local.client_port} \
               --data-dir ${local.data_volume_mount_path}/default.etcd \
-              --initial-advertise-peer-urls http://$${IP}:${local.peer_port} \
+              --initial-advertise-peer-urls http://$${HOSTNAME}:${local.peer_port} \
               --initial-cluster $${ETCD_INITIAL_CLUSTER} \
               --initial-cluster-state $${ETCD_INITIAL_CLUSTER_STATE}
       fi
   else
       echo "existing cluster not found"
-      ALL_PEER_ENDPOINTS="$${IP}=http://$${IP}:${local.peer_port}"
-      for peer_ip in $${PEER_IPS}; do
-          ALL_PEER_ENDPOINTS="$${ALL_PEER_ENDPOINTS}$${ALL_PEER_ENDPOINTS:+,}$${peer_ip}=http://$${peer_ip}:${local.peer_port}"
-      done
       save_member_id &
       echo "founding new cluster"
-      exec etcd --name $${IP} \
+      exec etcd --name $${POD_NAME} \
           --listen-peer-urls http://$${IP}:${local.peer_port} \
           --listen-client-urls http://$${IP}:${local.client_port},http://127.0.0.1:${local.client_port} \
-          --advertise-client-urls http://$${IP}:${local.client_port} \
+          --advertise-client-urls http://$${HOSTNAME}:${local.client_port} \
           --data-dir ${local.data_volume_mount_path}/default.etcd \
-          --initial-advertise-peer-urls http://$${IP}:${local.peer_port} \
-          --initial-cluster $${ALL_PEER_ENDPOINTS} \
+          --initial-advertise-peer-urls http://$${HOSTNAME}:${local.peer_port} \
+          --initial-cluster "$${POD_NAME}=http://$${HOSTNAME}:${local.peer_port}" \
           --initial-cluster-state new \
           --initial-cluster-token ${var.service_name}-cluster
   fi
@@ -89,7 +89,7 @@ locals {
   pre_stop_script = <<-EOT
   ${local.script_globals}
   MEMBER_ID=$(${local.get_member_id})
-  echo "Removing $${IP} from etcd cluster"
+  echo "removing $${POD_NAME} from cluster"
   ETCDCTL_ENDPOINT="$ALL_CLIENT_ENDPOINTS" etcdctl member remove $(${local.get_member_id})
   if [ $? -eq 0 ]; then
       rm -rf ${local.data_volume_mount_path}/*
