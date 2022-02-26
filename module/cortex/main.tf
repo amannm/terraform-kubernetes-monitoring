04f95cb1733fb2cd6e8af2b99a807e2fb6e63c71
Amann Malik <amannmalik@gmail.com>
2022-02-26 14:09:23 -0600

    increase limit

--- module/cortex/main.tf
+++ module/cortex/main.tf
@@ -57,7 +57,7 @@ module "ingester" {
   pod_resources = {
     cpu_min    = 50
     memory_min = 350
-    memory_max = 500
+    memory_max = 600
   }
   pod_lifecycle = {
     min_readiness_time = 30
