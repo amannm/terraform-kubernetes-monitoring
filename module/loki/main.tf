c23fcc9c6bebb55e4cc3f17ad75c967045202ccf
Amann Malik <amannmalik@gmail.com>
2022-02-27 15:16:49 -0600

    frontends to 1

--- module/loki/main.tf
+++ module/loki/main.tf
@@ -1,5 +1,5 @@
 locals {
-  query_frontend_replicas        = 2
+  query_frontend_replicas        = 1
   querier_component_name         = "querier"
   query_frontend_component_name  = "query-frontend"
   query_scheduler_component_name = "query-scheduler"
