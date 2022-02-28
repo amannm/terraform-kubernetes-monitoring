dc24d522d627d1904dd5bfab8120a8f41b50f15e
Amann Malik <amannmalik@gmail.com>
2022-02-27 23:55:20 -0600

    also grafana

--- module/grafana/main.tf
+++ module/grafana/main.tf
@@ -44,11 +44,18 @@ resource "kubernetes_config_map" "config_map" {
   }
 }
 
+resource "kubernetes_service_account" "service_account" {
+  metadata {
+    name      = var.service_name
+    namespace = var.namespace_name
+  }
+}
+
 module "grafana" {
   source               = "../common/stateful"
   namespace_name       = var.namespace_name
   service_name         = var.service_name
-  service_account_name = var.service_name
+  service_account_name = kubernetes_service_account.service_account.metadata[0].name
   replicas             = 1
   container_image      = var.container_image
   pod_resources = {
