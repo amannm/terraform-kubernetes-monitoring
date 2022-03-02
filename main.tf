1d236b57783c08fb9b19f2d561cde2184a007a93
Amann Malik <amannmalik@gmail.com>
2022-03-01 20:16:09 -0600

    disable

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ terraform {
     }
   }
 }
-locals {
-  partition_by_labels = {
-    component = distinct(flatten([for k, v in module.cortex.partition_by_labels : v if k == "component"]))
-  }
-}
+#locals {
+#  partition_by_labels = {
+#    component = distinct(flatten([for k, v in module.cortex.partition_by_labels : v if k == "component"]))
+#  }
+#}
 resource "kubernetes_namespace" "namespace" {
   metadata {
     name = var.namespace_name
@@ -43,38 +43,38 @@ module "kube_state_metrics" {
   container_image       = "k8s.gcr.io/kube-state-metrics/kube-state-metrics:v2.3.0"
   stateless_node_labels = var.stateless_node_labels
 }
+#
+#module "grafana_agent" {
+#  source                   = "./module/grafana-agent"
+#  namespace_name           = kubernetes_namespace.namespace.metadata[0].name
+#  resource_name            = "grafana-agent"
+#  agent_container_image    = "grafana/agent:latest"
+#  agentctl_container_image = "grafana/agentctl:latest"
+#  etcd_host                = module.shared_etcd.client_endpoint_host
+#  metrics_remote_write_url = module.cortex.remote_write_url
+#  logs_remote_write_url    = module.loki.remote_write_url
+#  partition_by_labels      = local.partition_by_labels
+#  stateless_node_labels    = var.stateless_node_labels
+#}
 
-module "grafana_agent" {
-  source                   = "./module/grafana-agent"
-  namespace_name           = kubernetes_namespace.namespace.metadata[0].name
-  resource_name            = "grafana-agent"
-  agent_container_image    = "grafana/agent:latest"
-  agentctl_container_image = "grafana/agentctl:latest"
-  etcd_host                = module.shared_etcd.client_endpoint_host
-  metrics_remote_write_url = module.cortex.remote_write_url
-  logs_remote_write_url    = module.loki.remote_write_url
-  partition_by_labels      = local.partition_by_labels
-  stateless_node_labels    = var.stateless_node_labels
-}
-
-module "cortex" {
-  source                = "./module/cortex"
-  namespace_name        = kubernetes_namespace.namespace.metadata[0].name
-  service_name          = "cortex"
-  service_port          = var.cortex_port
-  container_image       = "quay.io/cortexproject/cortex:v1.11.0"
-  storage_volume_size   = 1
-  etcd_host             = module.shared_etcd.client_endpoint_host
-  stateless_node_labels = var.stateless_node_labels
-}
-
-module "loki" {
-  source                = "./module/loki"
-  namespace_name        = kubernetes_namespace.namespace.metadata[0].name
-  service_name          = "loki"
-  service_port          = var.loki_port
-  container_image       = "grafana/loki:2.4.2"
-  storage_volume_size   = 1
-  etcd_host             = module.shared_etcd.client_endpoint_host
-  stateless_node_labels = var.stateless_node_labels
-}
+#module "cortex" {
+#  source                = "./module/cortex"
+#  namespace_name        = kubernetes_namespace.namespace.metadata[0].name
+#  service_name          = "cortex"
+#  service_port          = var.cortex_port
+#  container_image       = "quay.io/cortexproject/cortex:v1.11.0"
+#  storage_volume_size   = 1
+#  etcd_host             = module.shared_etcd.client_endpoint_host
+#  stateless_node_labels = var.stateless_node_labels
+#}
+#
+#module "loki" {
+#  source                = "./module/loki"
+#  namespace_name        = kubernetes_namespace.namespace.metadata[0].name
+#  service_name          = "loki"
+#  service_port          = var.loki_port
+#  container_image       = "grafana/loki:2.4.2"
+#  storage_volume_size   = 1
+#  etcd_host             = module.shared_etcd.client_endpoint_host
+#  stateless_node_labels = var.stateless_node_labels
+#}
